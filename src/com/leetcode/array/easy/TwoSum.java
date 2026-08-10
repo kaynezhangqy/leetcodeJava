0a8211da4044360005fc9f37a36262cc76e8022c
kayne.zhang <2990657021@qq.com>
2026-08-10 18:30:37 +0800

    1

--- src/com/leetcode/array/easy/TwoSum.java
+++ src/com/leetcode/array/easy/TwoSum.java
@@ -2,34 +2,34 @@ package com.leetcode.array.easy;
 
 /**
  * Two Sum - LeetCode Problem 1
- * 
- * Given an array of integers nums and an integer target, 
- * return indices of the two numbers such that they add up to target.
- * 
+ * <p>
+ * Given an array of integers nums and an integer target, return indices of the two numbers such
+ * that they add up to target.
+ * <p>
  * This implementation uses a brute force approach with O(n²) time complexity.
  */
 public class TwoSum {
 
-  /**
-   * Finds two numbers in the array that add up to the target using brute force.
-   * 
-   * @param nums the input array of integers
-   * @param target the target sum
-   * @return an array containing the indices of the two numbers, or empty array if no solution
-   */
-  public int[] twoSum(int[] nums, int target) {
-    // Brute force approach: check all possible pairs
-    for (int i = 0; i < nums.length; i++) {
-      for (int j = i + 1; j < nums.length; j++) {
-        if (nums[i] + nums[j] == target) {
-          return new int[]{i, j};
+    /**
+     * Finds two numbers in the array that add up to the target using brute force.
+     *
+     * @param nums   the input array of integers
+     * @param target the target sum
+     * @return an array containing the indices of the two numbers, or empty array if no solution
+     */
+    public int[] twoSum(int[] nums, int target) {
+        // Brute force approach: check all possible pairs
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
         }
-      }
+
+        // Return empty array if no solution found
+        return new int[]{};
     }
-    
-    // Return empty array if no solution found
-    return new int[]{};
-  }
 
     public int[] twoSum1(int[] nums, int target) {
         // Brute force approach: check all possible pairs
